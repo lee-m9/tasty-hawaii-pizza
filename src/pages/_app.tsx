34816a77c356b4a125d4d639a5a505e45ea2014e
Leela Kumar Malarpu <leela.kumar.m@oracle.com>
2023-02-05 21:32:38 +0530

    updated app tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,5 @@ import type { AppProps } from 'next/app'
 // import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-      <>
-          <Component {...pageProps} />
-          {/* <Analytics /> */}
-      </>
-  );
+  return <Component {...pageProps} />;
 }
